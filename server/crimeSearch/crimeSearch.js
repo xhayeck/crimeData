9164931024de6cad374c733c04932465f0035d49
Xavier <hayeck.xavier@gmail.com>
2017-01-24 18:15:48 -0800

    added comments and worked on api calls from server

--- server/crimeSearch/crimeSearch.js
+++ server/crimeSearch/crimeSearch.js
@@ -10,7 +10,7 @@ module.exports = {
     let googleGeocodeApi = process.env.googleGeocodeApi; //google-geocoding api
     let crimes = [];
     if(address.street) { //let's us know if user wants a specific location as the search point
-      request('https://maps.googleapis.com/maps/api/geocode/json?address='
+      request('https://maps.googleapis.com/maps/api/geocode/json?address=' //Calls google api to change address to geographic coordinates
                 + address.street
                 + ','
                 + address.city
@@ -25,28 +25,43 @@ module.exports = {
             body = JSON.parse(body);
             let latitude = body.results[0].geometry.location.lat;
             let longitude = body.results[0].geometry.location.lng;
-            // let query = "?$where="
-            //             + "date >= '" + dates.start + "'"
-            //             + "AND date <= '" + dates.end + "'"
-            //             + "AND within_circle(location,"+ latitude + "," + longitude + "," + distance.feet + ")"
-            //             + "&$order=date DESC";
-            //             + "&$limit=100000";
-            // console.log('query: ', query);
             let sources = list[address.state];
-            let count = 0;
-            sources.forEach(function(city) {
-              request(city.apiUrl + query, function(error, response, body) {
-                if(error) {
-                  res.send(error);
-                }
-                if(body) {
-                  crimes.push(body);
-                  count++;
-                }
-                if(count === sources.length) {
-                  res.send(crimes);
-                }
-              });
+            let cityCount = 0; //Setting up count to make sure every city in the state is iterated through
+            let apiCount = 0; //Setting up count to make sure every api in the city is iterated through
+            let apiSize = 0; //Sets up total of api's needed to cycle through
+            sources.forEach(function(city) { //To cycle through each city in the state
+              let query = "?$where="
+                          + city['query']['startDate']
+                          + dates.start + "'"
+                          + city['query']['endDate']
+                          + dates.end + "'"
+                          + city['query']['radius']
+                          + latitude + ","
+                          + longitude + ","
+                          + distance.feet + ")"
+                          + city['query']['order'];
+              for(let apiUrl in city['apiUrl']) { //To cycle through each api in a city
+                apiSize++;
+                request(city['apiUrl'][apiUrl] + query, function(error, response, body) {
+                  console.log('query: ', city['apiUrl'][apiUrl] + query);
+                  if(error) {
+                    res.send(error);
+                  }
+                  if(body) {
+                    crimes.push(body);
+                  }
+                  if(response) {
+                    apiCount++;
+                  }
+                  if(cityCount === sources.length && apiCount === apiSize) { //To make sure all api calls are finished before sending back to client
+                    res.send(crimes);
+                  }
+                });
+              }
+              if(response) {
+                cityCount++;
+              }
+
             });
           }
         });
--- server/crimeSearch/stateList.js
+++ server/crimeSearch/stateList.js
@@ -9,13 +9,14 @@ module.exports = {
       "apiUrl": { //Key(s) represent date(s) that that particular dataset has
         "2003ToPresent": "https://data.sfgov.org/resource/cuks-n6tp.json"
       },
-      "url": "https://data.sfgov.org/browse?category=Public+Safety&limitTo=datasets&tags=crime"
-      // "query": "?$where="
-      //             + "date >= '" + dates.start + "'"
-      //             + "AND date <= '" + dates.end + "'"
-      //             + "AND within_circle(location,"+ latitude + "," + longitude + "," + distance.feet + ")"
-      //             + "&$order=date DESC"
-      //             + "&$limit=100000"
+      "url": "https://data.sfgov.org/browse?category=Public+Safety&limitTo=datasets&tags=crime",
+      "query": {
+        "startDate":"date >= '",
+        "endDate": "AND date <= '",
+        "radius": "AND within_circle(location,",
+        "order": "&$order=date DESC",
+        "limit": "&$limit=100000"
+      }
     }
   ],
   "IL": [
@@ -24,7 +25,14 @@ module.exports = {
       "apiUrl": { //Key(s) represent date(s) that that particular dataset has
         "2001ToPresent": "https://data.cityofchicago.org/resource/6zsd-86xi.json"
       },
-      "url": "https://data.cityofchicago.org/browse?category=Public+Safety&limitTo=datasets"
+      "url": "https://data.cityofchicago.org/browse?category=Public+Safety&limitTo=datasets",
+      "query": {
+        "startDate":"",
+        "endDate": "",
+        "radius": "",
+        "order": "",
+        "limit": ""
+      }
     }
   ],
   "WA": [
@@ -33,7 +41,14 @@ module.exports = {
       "apiUrl": { //Key(s) represent date(s) that that particular dataset has
         "Aug2011ToPresent": "https://data.seattle.gov/resource/y7pv-r3kh.json"
       },
-      "url": "https://data.seattle.gov/browse?category=Public+Safety&limitTo=datasets&tags=police+report"
+      "url": "https://data.seattle.gov/browse?category=Public+Safety&limitTo=datasets&tags=police+report",
+      "query": {
+        "startDate":"occurred_date_or_date_range_start ",
+        "endDate": "AND occurred_date_range_end ",
+        "radius": "AND within_circle(location,",
+        "order": "&$order=date DESC",
+        "limit": "&$limit=100000"
+      }
     }
   ]
 };
